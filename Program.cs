72bba9a47dac1bdef1102ca6dc74eb0d16490d0f
ShendygaevDmitriy <129860994+ShendygaevDmitriy@users.noreply.github.com>
2023-04-10 22:58:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+
+string s = Console.ReadLine(); 
+int i;
+
+if (int.TryParse(s, out i)) {
+    System.Console.WriteLine($"Квадрат числа {i} = {Math.Pow(i,2)}");
+}
+else{
+    System.Console.WriteLine("Упс");
+}
